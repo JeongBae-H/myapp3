560a372e4861e146ddde5b98fc55a18be5cfefea
Hwang JeongBae <hjb0403@naver.com>
2024-07-29 17:51:46 +0900

    ok

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -6,16 +6,30 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         // Student student = new Student();
-        // // System.out.println(student.getStudentId());
-        // // System.out.println(student.getStudentName());
-        // // student.setStudentId("1");
-        // // student.setStudentName("이순신");
+        // System.out.println(student.getStudentId());
+        // System.out.println(student.getStudentName());
+        // student.setStudentId("1");
+        // student.setStudentName("이순신");
+
+        // ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+        // Student student = context.getBean("student", Student.class);
+        // student.setStudentId("2");
+        // student.setStudentName("강감찬");
+        // System.out.println(student.getStudentName());
+
+        // Address address = new Address();
+        // address.setLocation("Seoul");
+        // Employee employee = new Employee();
+        // employee.setName("세종대왕");
+        // employee.setAddress(address);
+        // employee.displayInfo();
 
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        Employee employee = context.getBean("employee", Employee.class);
+        Address address = context.getBean("address", Address.class);
+        employee.setAddress(address);
+        employee.displayInfo();
 
-        Student student = context.getBean("student", Student.class);
-        student.setStudentId("2");
-        student.setStudentName("강감찬");
-        System.out.println(student.getStudentName());
     }
 }
