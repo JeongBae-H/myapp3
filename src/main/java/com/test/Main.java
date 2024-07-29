9ee689151e62a8a4e09405bc98e6f97bc46c0834
Hwang JeongBae <hjb0403@naver.com>
2024-07-29 15:04:13 +0900

    ok

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -3,6 +3,6 @@ package com.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Have a good day3");
+        System.out.println("Have a good day2");
     }
 }
